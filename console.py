c7b08e4ab6f9bb7b42a8a5711ce46a002f5e39b6
francotaind <francotaind00@gmail.com>
2024-02-12 04:22:35 +0300

    minor changes

--- console.py
+++ console.py
@@ -3,14 +3,16 @@
 import cmd
 import re
 from shlex import split
+
 from models import storage
+from models.amenity import Amenity
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
 from models.city import City
 from models.place import Place
-from models.amenity import Amenity
 from models.review import Review
+from models.state import State
+from models.user import User
+
 
 
 def parse(arg):
